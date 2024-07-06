3f0bf1455de485e2a39e1900b0761b9bbc4e5dfb
justin <justinmwathi32@gmail.com>
2024-07-06 12:58:12 +0300

    code-challenge done!

--- server/models.py
+++ server/models.py
@@ -21,9 +21,13 @@ class Restaurant(db.Model, SerializerMixin):
     address = db.Column(db.String)
 
     # add relationship
-
+    restaurant_pizzas=db.relationship("RestaurantPizza",back_populates='restaurant',cascade='all, delete-orphan')
     # add serialization rules
+    serialize_rules=('-restaurant_pizzas.restaurant',)
 
+    #Association proxy
+    pizzas=association_proxy('restaurant_pizzas','pizza',creator=lambda pizza_obj: RestaurantPizza(pizza_obj))
+    
     def __repr__(self):
         return f"<Restaurant {self.name}>"
 
@@ -36,9 +40,12 @@ class Pizza(db.Model, SerializerMixin):
     ingredients = db.Column(db.String)
 
     # add relationship
-
+    restaurant_pizzas=db.relationship('RestaurantPizza', back_populates='pizza',cascade='all, delete-orphan')
     # add serialization rules
+    serialize_rules=('restaurant_pizzas.pizza',)
 
+    #Association proxy
+    restaurants=association_proxy("restaurant_pizzas","restaurant",creator=lambda restaurant_obj:RestaurantPizza(restaurant_obj))
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
 
@@ -48,12 +55,18 @@ class RestaurantPizza(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
-
+    restaurant_id=db.Column(db.Integer,db.ForeignKey("restaurants.id"))
+    pizza_id=db.Column(db.Integer,db.ForeignKey("pizzas.id"))
     # add relationships
-
+    restaurant=db.relationship('Restaurant',back_populates='restaurant_pizzas')
+    pizza=db.relationship('Pizza',back_populates='restaurant_pizzas')
     # add serialization rules
-
+    serialize_rules=('-restaurant.restaurant_pizzas','-pizza.restaurant_pizzas',)
     # add validation
-
+    @validates('price')
+    def validates_price(self,key,price):
+        if  price < 1 or price > 30:
+            raise ValueError("Price must between 1 and 30!")
+        return price 
     def __repr__(self):
         return f"<RestaurantPizza ${self.price}>"
